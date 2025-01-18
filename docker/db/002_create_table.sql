d1029301db5c9579750500ddf1abccdee90e4e78
rjacobs1969 <rjacobs@atrapalo.com>
2025-01-18 19:22:40 +0100

    Update Docker setup to initialize database and create the table with necessary privileges

---
+++ docker/db/002_create_table.sql
@@ -0,0 +1,15 @@
+USE `produce`;
+
+DROP TABLE IF EXISTS `produce`;
+
+CREATE TABLE `produce`
+(
+    `id`               int          NOT NULL AUTO_INCREMENT,
+    `name`             VARCHAR(255) NOT NULL,
+    `type`             VARCHAR(16)  NOT NULL,
+    `weight_in_grams`  INT          NOT NULL,
+    PRIMARY KEY (id),
+    INDEX         by_type (`type` ASC),
+    INDEX         by_name (`name` ASC)
+) ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
