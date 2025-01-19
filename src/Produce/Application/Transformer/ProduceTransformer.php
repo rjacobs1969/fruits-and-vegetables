0ee833b416d30d1d107d244929451e9ccefe7dd3
rjacobs1969 <rjacobs@atrapalo.com>
2025-01-19 02:14:32 +0100

    Add transform method to ProduceTransformer for handling search requests

--- src/Produce/Application/Transformer/ProduceTransformer.php
+++ src/Produce/Application/Transformer/ProduceTransformer.php
@@ -6,10 +6,21 @@ namespace App\Produce\Application\Transformer;
 
 use App\Produce\Domain\Collection\ProduceCollection;
 use App\Produce\Domain\Entity\Produce;
+use App\Shared\Domain\SearchRequest;
 use App\Shared\Domain\WeightUnit;
 
 class ProduceTransformer
 {
+    public function transform(ProduceCollection $collection, SearchRequest $searchRequest): array
+    {
+        $displayWeightUnit = $searchRequest->weightUnit();
+        if ($searchRequest->id() !== null && $collection->count() === 1) {
+            return $this->transformProduce($collection->find($searchRequest->id()), $displayWeightUnit);
+        }
+
+        return $this->transformCollection($collection, $displayWeightUnit);
+    }
+
     public function transformCollection(ProduceCollection $collection, WeightUnit $displayWeightUnit): array
     {
         $result = [];
