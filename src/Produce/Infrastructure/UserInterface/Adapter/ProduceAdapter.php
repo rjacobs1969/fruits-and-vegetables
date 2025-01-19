3baa9ae5b8695a39d2a117bd9fac9f8f02f25531
rjacobs1969 <rjacobs@atrapalo.com>
2025-01-19 14:21:46 +0100

    Add ProduceAdapter for adapting produce data to domain entities

---
+++ src/Produce/Infrastructure/UserInterface/Adapter/ProduceAdapter.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Produce\Infrastructure\UserInterface\Adapter;
+
+use App\Produce\Domain\Collection\ProduceCollection;
+use App\Produce\Domain\Entity\Produce;
+
+class ProduceAdapter
+{
+    public function adaptToDomain(array $data): Produce
+    {
+        return new Produce(
+            isset($data['id']) ? (int) $data['id'] : null,
+            (string) $data['name'] ?? '',
+            (string) $data['type'] ?? '',
+            (int)    $data['quantity'] ?? 0,
+            (string) $data['unit'] ?? 'g'
+        );
+    }
+
+    public function adaptFromArray(array $data): ProduceCollection
+    {
+        $collection = new ProduceCollection();
+        foreach ($data as $item) {
+            $collection->add($this->adaptToDomain($item));
+        }
+
+        return $collection;
+    }
+}
