6b258790481fd09e2e747d1a49c37a8c92830f71
rjacobs1969 <rjacobs@atrapalo.com>
2025-01-20 19:16:33 +0100

    improve ImportProduceCommandTest and update test data

--- tests/Infrastructure/ImportProduceCommandTest.php
+++ tests/Infrastructure/ImportProduceCommandTest.php
@@ -3,7 +3,6 @@
 namespace App\Test\Infrastructure;
 
 use App\Produce\Infrastructure\Command\ImportProduceCommand;
-use App\Produce\Infrastructure\Persistence\Database\Repository\ProduceDbalRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -18,7 +17,6 @@ class ImportProduceCommandTest extends WebTestCase
     {
         self::ensureKernelShutdown();
         $this->command = self::getContainer()->get(ImportProduceCommand::class);
-        $this->cleanUp();
     }
 
     public function testImportOk(): void
@@ -27,23 +25,5 @@ class ImportProduceCommandTest extends WebTestCase
         $tester->execute(['fileName' => self::TEST_DATA_FILE]);
 
         $this->assertEquals(Command::SUCCESS, $tester->getStatusCode());
-    }
-
-    public function testFailImportTheSameDataTwice(): void
-    {
-        $tester = new CommandTester($this->command);
-        $tester->execute(['fileName' => self::TEST_DATA_FILE]);
-        $tester->execute(['fileName' => self::TEST_DATA_FILE]); // execute twice
-
-        $this->assertEquals(Command::FAILURE, $tester->getStatusCode());
-    }
-
-    private function cleanup(): void
-    {
-        $testDatabaseRepository = self::getContainer()->get(ProduceDbalRepository::class);
-
-        for ($id = 0; $id < 4; $id++) {
-            $testDatabaseRepository->delete($id);
-        }
     }
 }
